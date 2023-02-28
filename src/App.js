d8c57aa56f76f7f3f0af91f0fc33990a7fedf8e2
Keenan Urdiales <Keenan-Urdiales@Keenans-MacBook-Air.local>
2023-02-27 17:59:02 -0700

    resume

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          test
-        </p>
+     
+      <p>navbar</p>
+      <p>footer</p>
+      <p>resume</p>
       
         <a
       
