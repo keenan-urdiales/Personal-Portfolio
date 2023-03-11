49ffd2d8e5027dc7078ddf08703ffa5c3bc753a1
Keenan Urdiales <keenanurdiales@icloud.com>
2023-03-10 18:43:32 -0700

    projects page

--- src/App.js
+++ src/App.js
@@ -14,11 +14,8 @@ function App() {
         <p>Hobbies and cool shiiat</p>
         <p>Photos</p>
         <p>Navbar</p>
-      
-       
-
-    
-       
+        <p>projects page</p>
+     
         <a
       
           className="App-link"
